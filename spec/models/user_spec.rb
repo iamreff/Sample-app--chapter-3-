d8db67e6bf8a01ca13522e94c8723e4e02d32a9f
John Refford <jrefford@gmail.com>
2012-05-24 22:41:28 -0400

    Just before lesson 7

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -46,7 +46,8 @@ describe User do
 
   describe "when email format is not valid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+			foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
 	@user.should_not be_valid
