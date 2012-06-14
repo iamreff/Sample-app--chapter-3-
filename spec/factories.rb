63ca264a6bd077ce3860fd0e674660eb046026aa
John Refford <jrefford@gmail.com>
2012-06-14 07:55:39 -0400

    Finish updating users

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    name "John Refford"
-    email "john@example.com"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "Person_#{n}@example.com" }
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
 
